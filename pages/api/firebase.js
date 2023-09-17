1751d01b2d2dd291510c9d92026205628822442b
NeoDitjoe <ditjoeneo03@gmail.com>
2023-09-17 18:40:45 +0200

    signed up or not

--- pages/api/firebase.js
+++ pages/api/firebase.js
@@ -1,14 +1,13 @@
-import Register from "@/components/register"
-import Link from "next/link"
-import { useRef } from "react"
 import EmailAndPassword, { app , googleSignIn, githubSignIn} from "@/Config/firebase"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
 import LoginWithEmailAndPassword from "@/Config/loginfirebaseEmail"
+import { useState } from "react"
 
 export default function FirebaseAuth(){
 
     const router  = useRouter()
+    const [ isRegistered, setIsRegistered ] = useState(true)
 
     useEffect(() => {
         let token = sessionStorage.getItem('Token')
@@ -20,8 +19,17 @@ export default function FirebaseAuth(){
 
     return (
         <>
-            <EmailAndPassword />
-            <LoginWithEmailAndPassword/>
+            {
+                isRegistered ?  <EmailAndPassword /> :
+
+                <LoginWithEmailAndPassword/>
+            }
+
+            {isRegistered ? 'have an account ? ' : "Don't have an account ? "}
+            <button onClick={() => setIsRegistered(!isRegistered)}>{ isRegistered ? 'Login' : 'Sign up'}</button>
+
+            <br></br>
+            <br></br>
             <button onClick={githubSignIn}>Github</button>
             <button onClick={googleSignIn}>Google</button>
         
